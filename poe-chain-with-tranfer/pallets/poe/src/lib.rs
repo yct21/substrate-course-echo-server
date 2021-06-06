abd0cecacf4f18aa12ee9cf18d86f408f04483d5
Chutian Yang <yct21@12tcy.com>
2021-06-06 23:22:10 +0800

    Yet another task

---
+++ poe-chain-with-tranfer/pallets/poe/src/lib.rs
@@ -0,0 +1,121 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// https://substrate.dev/docs/en/knowledgebase/runtime/frame
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+    use frame_system::pallet_prelude::*;
+    use sp_std::vec::Vec;
+
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+    }
+
+    #[pallet::event]
+    #[pallet::metadata(T::AccountId = "AccountId")]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// a proof has been claimed. [who, claim]
+        ClaimCreated(T::AccountId, Vec<u8>),
+        /// a claim is revoked by the owner. [who, claim]
+        ClaimRevoked(T::AccountId, Vec<u8>),
+		/// a claim is transfered by the owner. [sender, receiver, claim]
+        ClaimTransfered(T::AccountId, T::AccountId, Vec<u8>),
+    }
+
+    #[pallet::error]
+    pub enum Error<T> {
+        /// The proof has already been claimed.
+        ProofAlreadyClaimed,
+        /// The proof does not exist, so it cannot be revoked.
+        NoSuchProof,
+        /// The proof is claimed by another account, so caller can't revoke it.
+        NotProofOwner,
+		/// Receiver of a transfer is sender themself
+		TransferToSelf,
+    }
+
+    #[pallet::storage]
+    pub(super) type Proofs<T: Config> =
+        StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber), ValueQuery>;
+
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::weight(1_000)]
+        pub(super) fn create_claim(
+            origin: OriginFor<T>,
+            proof: Vec<u8>,
+        ) -> DispatchResultWithPostInfo {
+            let sender = ensure_signed(origin)?;
+
+            ensure!(
+                !Proofs::<T>::contains_key(&proof),
+                Error::<T>::ProofAlreadyClaimed
+            );
+
+            let current_block = <frame_system::Module<T>>::block_number();
+
+            Proofs::<T>::insert(&proof, (&sender, current_block));
+
+            Self::deposit_event(Event::ClaimCreated(sender, proof));
+
+            Ok(().into())
+        }
+
+        #[pallet::weight(10_000)]
+        fn revoke_claim(origin: OriginFor<T>, proof: Vec<u8>) -> DispatchResultWithPostInfo {
+            let sender = ensure_signed(origin)?;
+
+            ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
+
+            let (owner, _) = Proofs::<T>::get(&proof);
+
+            ensure!(sender == owner, Error::<T>::NotProofOwner);
+
+            Proofs::<T>::remove(&proof);
+
+            Self::deposit_event(Event::ClaimRevoked(sender, proof));
+
+            Ok(().into())
+        }
+
+        #[pallet::weight(10_000)]
+        fn transfer_claim(origin: OriginFor<T>, proof: Vec<u8>, to: T::AccountId) -> DispatchResultWithPostInfo {
+            let sender = ensure_signed(origin)?;
+
+			ensure!(sender != to, Error::<T>::TransferToSelf);
+            ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
+
+            let (owner, _) = Proofs::<T>::get(&proof);
+
+            ensure!(sender == owner, Error::<T>::NotProofOwner);
+
+            let current_block = <frame_system::Module<T>>::block_number();
+
+            Proofs::<T>::insert(&proof, (&to, current_block));
+
+            Self::deposit_event(Event::ClaimTransfered(sender, to, proof));
+
+            Ok(().into())
+        }
+    }
+}
